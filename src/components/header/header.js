e3d1d56606c878cda38d03a309d3906339aadd62
Kevin Choi <kevinyoungchoi@gmail.com>
2021-04-28 23:15:39 -0400

    updated github and linkedin urls

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -25,8 +25,8 @@ export default class Header extends Component {
             <hr />
             <ul className="social">
                 <li><a href="https://www.facebook.com/choik305"><i className="fa fa-facebook" /></a></li>
-                <li><a href="https://www.linkedin.com/in/kevinchoi305/"><i className="fa fa-linkedin" /></a></li>
-                <li><a href="https://github.com/choik305"><i className="fa fa-github" /></a></li>
+                <li><a href="https://www.linkedin.com/in/kevinyc/"><i className="fa fa-linkedin" /></a></li>
+                <li><a href="https://github.com/kevinyc-dri"><i className="fa fa-github" /></a></li>
             </ul>
           </div>
         </div>
