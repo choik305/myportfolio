9cfdc3c097538368c3e34964230e45a9668cf4df
Kevin Choi <kevinyoungchoi@gmail.com>
2021-05-02 09:19:55 -0400

    added script

---
+++ public/precache-manifest.fef55d44b7b45db2102170ff41857375.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db750fdf77e05518095555a8894ad44b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06d4b195e8cecd11cc86",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "463695d8e1ee28bf482c",
+    "url": "/static/js/2.223480c7.chunk.js"
+  },
+  {
+    "revision": "06d4b195e8cecd11cc86",
+    "url": "/static/js/main.889ca9b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
