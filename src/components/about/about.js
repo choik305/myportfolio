90630428045e1ed161e332e1a4d16083ececfbc4
Kevin Choi <kevinyoungchoi@gmail.com>
2021-05-13 00:10:43 -0400

    refactor about code to funcitonal component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,14 +10,14 @@ function About() {
             </div>
             <div className="nine columns main-col">
               <h2>About Me</h2>
-              <p>I am a software engineering student, husband and father with 10+ years of experience in various industries. I have a passion for innovation, educational technology and Open-source. I am an evangelist for software engineering and strong company culture.</p>
+              <p>I am a software engineering student, husband and father with many years of experience in various industries. I have a passion for educational technology and Open-source technology. I am an evangelist for software engineering and strong company culture.</p>
               <div className="row">
                 <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
                     <span>Kevin Choi</span>
                     <br />
-                    <span>Miami, FL US</span>
+                    <span>Miami, FL</span>
                     <br />
                     <span>kevinyoungchoi@gmail.com</span>
                   </p>
