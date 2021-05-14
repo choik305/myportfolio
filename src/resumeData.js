947bb5092ec0327e706ac19851ec2f65554f69ef
Kevin Choi <kevinyoungchoi@gmail.com>
2021-05-13 20:54:17 -0400

    added more content to my about page

--- src/resumeData.js
+++ src/resumeData.js
@@ -2,6 +2,6 @@ let resumeData = {
   "imagebaseurl": "https://github.com/kevinyc-dri/2ndPortfolio.git",
   "name": "Kevin Choi",
   "role": "Software Engineer",
-  "roleDescription": " I build frontend and backend applications. I have a passion for teaching code. On my free time, I skateboard with my son!",
+  "roleDescription": " I build frontend and backend applications. I have a passion for building and teaching code. On my free time, I skateboard with my son!",
 }
 export default resumeData
