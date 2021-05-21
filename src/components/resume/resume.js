f875e95dca6be516a85a97d97a8fec5c4f503054
Kevin Choi <kevinyoungchoi@gmail.com>
2021-05-21 01:15:55 -0400

    fixed some css

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -3,15 +3,15 @@ export default class Resume extends Component {
   render() {
     return (
       <>
-        <section id="resume">
+        <section id="resume">Î
           <div className="row education">
             <div className="three columns header-col">
-              <h1><span>Education</span></h1>
+              <h1><span className="ed-title">Education</span></h1>
             </div>
             <div className="nine columns main-col">
               <div className="row item">
                 <div className="twelve columns">
-                  <h2>Boca Code</h2>
+                  <h3 className="school-title">Boca Code</h3>
                   <p className="info">Software Engineering Career Course <span>•</span><em className="date">June 2021</em></p>
                   <p>
                     Boca Code’s Software Engineering Career Course focuses on the latest technology which include JavaScript, Node.js, HTML, CSS, React, Firebase and many more. On top of technical skills, Boca Code teaches soft skills that hone in on professional development, agile methodologies, team work, communication and collaboration.
@@ -20,8 +20,8 @@ export default class Resume extends Component {
               </div> {/* item end */}
               <div className="row item">
                 <div className="twelve columns">
-                  <h3>University of Nevada, Las Vegas</h3>
-                  <p className="info">B.A. Hotel Management <span>•</span> <em className="date">June 2008</em></p>
+                  <h3 className="school-title">University of Nevada, Las Vegas</h3>
+                  <p className="info">B.S. Hotel Management <span>•</span> <em className="date">June 2008</em></p>
                   <p>
                     UNLV is a doctoral-degree-granting institution of more than 30,000 students and 3,500 faculty and staff that is recognized as “very high research activity” by the Carnegie Classification of Institutions of Higher Education.</p>
                 </div>
